9eabb0133009f5939ceb0ce5c1744ed0b82a22b0
gibba42 <alastairgibb.kent@gmail.com>
2026-05-06 22:51:27 +0100

    Added account registration views.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import redirect, render
+
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.user.is_authenticated:
+        messages.info(request, "You are already logged in.")
+        return redirect('profile')
+    
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(
+                request,
+                "Account created successfully. Welcome to Book Tracker!"
+            )
+            return redirect('profile')
+        else:
+            form = UserRegisterForm()
+
+        return render(request, 'accounts/register.html', {'form': form})
+    
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
 
 # Create your views here.
