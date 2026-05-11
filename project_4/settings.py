1f6b9d18e8e1bf12e1fc40132d5daeac240a8694
gibba42 <alastairgibb.kent@gmail.com>
2026-05-11 19:47:07 +0100

    Added settings.py to gitignore, and set debug = false.

--- project_4/settings.py
+++ project_4/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -75,17 +75,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project_4.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 DATABASES = {
     'default':
